b278f1814577bc64018ab1967d70597977381ed7
bethanie.garfin <bethanie.garfin@autotrader.co.uk>
2020-05-26 08:05:30 +0100

    Create timetable column component

--- app/src/index.js
+++ app/src/index.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import TimetableColum from './components/timetableColumn.js'
 import './css/index.css';
-import {timetableContainer, column, title, rowTitle, day} from './css/app.css';
+import style from './css/app.css';
 
+
+const testColumn = [
+    {name: style.title, content: '19:45 to 21:15'},
+    {name: "bookingSlot", content: ''},
+    {name: "bookingSlot", content: ''},
+    {name: "bookingSlot", content: ''},
+    {name: "bookingSlot", content: ''},
+    {name: "bookingSlot", content: ''},
+    {name: "bookingSlot", content: ''}
+]
 class App extends React.Component {
     render() {
         return(
             <div>
-            <p id={day}>Monday 25th May</p>
-            <div id={timetableContainer}>
-                <div className={column}>
-                    <p className={title}>Tables</p>
-                    <p className={rowTitle}>Training Room Table 1</p>
-                    <p className={rowTitle}>Training Room Table 2</p>
-                    <p className={rowTitle}>Training Room Table 3</p>
-                    <p className={rowTitle}>Training Room Table 4</p>
-                    <p className={rowTitle}>Dragon Match Room</p>
-                    <p className={rowTitle}>George Match Room</p>
+            <p id={style.day}>Monday 25th May</p>
+            <div id={style.timetableContainer}>
+                <div className={style.column}>
+                    <p className={style.title}>Tables</p>
+                    <p className={style.rowTitle}>Training Room Table 1</p>
+                    <p className={style.rowTitle}>Training Room Table 2</p>
+                    <p className={style.rowTitle}>Training Room Table 3</p>
+                    <p className={style.rowTitle}>Training Room Table 4</p>
+                    <p className={style.rowTitle}>Dragon Match Room</p>
+                    <p className={style.rowTitle}>George Match Room</p>
                 </div>
-                <div className={column}>
-                    <p className={title}>12:00 to 13:30</p>
-                    <div className="bookingSlot"></div>
-                    <div className="bookingSlot"></div>
-                    <div className="bookingSlot"></div>
-                    <div className="bookingSlot"></div>
-                    <div className="bookingSlot"></div>
-                    <div className="bookingSlot"></div>
-                </div>
-                <div className={column}>
-                    <p className={title}>13:45 to 15:15</p>
+                <div className={style.column}>
+                    <p className={style.title}>12:00 to 13:30</p>
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
@@ -37,8 +39,8 @@ class App extends React.Component {
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
                 </div>
-                <div className={column}>
-                    <p className={title}>15:30 to 17:00</p>
+                <div className={style.column}>
+                    <p className={style.title}>13:45 to 15:15</p>
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
@@ -46,8 +48,8 @@ class App extends React.Component {
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
                 </div>
-                <div className={column}>
-                    <p className={title}>18:00 to 19:30</p>
+                <div className={style.column}>
+                    <p className={style.title}>15:30 to 17:00</p>
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
@@ -55,8 +57,8 @@ class App extends React.Component {
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
                 </div>
-                <div className={column}>
-                    <p className={title}>19:45 to 21:15</p>
+                <div className={style.column}>
+                    <p className={style.title}>18:00 to 19:30</p>
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
@@ -64,6 +66,7 @@ class App extends React.Component {
                     <div className="bookingSlot"></div>
                     <div className="bookingSlot"></div>
                 </div>
+                <TimetableColum rows={testColumn}/>
             </div>
             </div>
         )
